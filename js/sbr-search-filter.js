83f3fdf075af7c14bb420ae853a91565186240df
RhodGriffith <rhodri.griffith@ons.gov.uk>
2018-10-05 10:50:04 +0100

    Add backup UI prototypes

--- js/sbr-search-filter.js
+++ js/sbr-search-filter.js
@@ -4,10 +4,24 @@ $(function () {
             $("#enterprise").hide();
             $("#local-unit-one").hide();
             $("#ch-unit-one").hide();
+            $("#ch-unit-two").hide();
+            $("#lu-unit-one").hide();
+            $("#lu-unit-two").hide();
+            $("#lu-unit-three").hide();
+            $("#lu-unit-four").hide();
+            $("#lu-unit-five").hide();
+            $("#pagination").hide();
         } else {
           $("#enterprise").show();
           $("#local-unit-one").show();
           $("#ch-unit-one").show();
+          $("#ch-unit-two").show();
+          $("#lu-unit-one").show();
+          $("#lu-unit-two").show();
+          $("#lu-unit-three").show();
+          $("#lu-unit-four").show();
+          $("#lu-unit-five").show();
+          $("#pagination").show();
         }
     });
 });
